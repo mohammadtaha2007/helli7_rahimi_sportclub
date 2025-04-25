29db5254b81dc96595d022679174e173e1d0312a
mohammadtaha2007 <167072349+mohammadtaha2007@users.noreply.github.com>
2025-04-25 14:26:49 +0330

    Add heder.php
    here i add including header part of site including

---
+++ including/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iconic Team Template</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Header Part -->
+    <header class="main-header">
+        <div class="top-row">
+            <div class="container t-flex">
+                <p>This Site Is About All Time Football Iconic Team </p>
+
+                <ul class="c-flex">
+                    <li> <a href="" class="eng">ENG</a> </li> 
+                    <div>|</div>
+                    <li> <a href="" class="fa">FA</a> </li>
+                    <div>|</div>
+                    <li class="enjoy"> Enjoy This Team </li>
+                </ul> 
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="mid-row">
+            <div class="container t-flex">
+            <div class="t-flex">
+                <img class="search-logo" src="images/Search.png" alt="">
+                <input type="search" placeholder="Search About Your Iconic Player..." class="serach-box">
+            </div>
+
+            <div class="t-flex">
+                <a href="MainPage.php"><img class="site-logo" src="images/sitelogo.png" alt=""></a>
+            </div>
+
+            <div>
+                <div class="t-flex">
+                    <img src="images/person.png" class="person" alt="">
+                    <a href="" class="sign-in">Sign in</a>
+                    <div>|</div>
+                    <img src="images/shoppingbasket.png" class="shop-basket" alt="">
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <div class="bottom-row">
+            <div class="container t-flex">
+                <ul class="t-flex nav-bar">
+                    <li class="nav">
+                        <a href="SecondPage.php">Browse Categories</a>                    
+                    </li>
+
+                    <li class="nav">
+                        <a href="SecondPage.php">Products</a>
+                    </li>
+
+                    <li class="nav">
+                        <a href="SecondPage.php">Blog</a>
+                    </li>
+
+                    <li class="nav">
+                        <a href="SecondPage.php">Contact</a>
+                    </li>
+                </ul>
+
+                <div class="t-flex nav">
+                    <a href="">Customer Cure : +989370307618</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- End Header Part -->
